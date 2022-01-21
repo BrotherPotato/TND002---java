db9557111d846df20b4510775be2094ea9883f20
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-01-21 11:24:29 +0100

    lesson 1

--- Lessons/src/lesson1/Human.java
+++ Lessons/src/lesson1/Human.java
@@ -1,7 +1,81 @@
 package lesson1;
 
 public class Human {
-
+	
+	private String name;
+	private int age;
+	private double weight;
+	
+	private static boolean outputFormat = false;
+	
+	private boolean complete = false;
+	
+	public static final boolean FULL = true;
+	public static final boolean SHORT = false;
+	
+	
+	public Human(String name) {
+		 this.name = name;
+	}
+	public Human(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	public Human(String name, int age, double weight) {
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+		this.complete = true;
+	}
+	public static String setOutputFormat (boolean bArg) {
+		outputFormat = bArg;
+		if(outputFormat) {
+			return "Full format";
+		} else {
+			return "Short format";
+		}
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String comparison(Human h) {
+		int v1 = this.name.compareTo(h.name);
+		int v2 = compareTo(h.age);
+		int v3 = compareTo(h.age);
+		
+		return "Text";
+	}
+	public int compareTo(int age) {
+		if(this.age == age) {
+			return 0;
+		} else if(this.age > age) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+	public int compareTo(double weight) {
+		if(this.weight == weight) {
+			return 0;
+		} else if(this.weight > weight) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+	public String getName() {
+		return this.name;
+	}
+	public String toString() {
+		if(outputFormat && complete) {
+			return "sas";
+		} else {
+			return "Name. " + this.name;
+		}
+	}
+	
+	
+	/*
 	private String name;
 	private int age;
 	private double weight; 
@@ -67,5 +141,5 @@ public class Human {
 			return "Name: " + name;
 		}
 	}
-	
+	*/
 }

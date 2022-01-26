b23345191eb52178df1668594ff9314b3a019fc5
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-01-26 10:38:28 +0100

    doc

--- LectureCode/src/test/Name.java
+++ LectureCode/src/test/Name.java
@@ -1,10 +1,33 @@
 package test;
 
+/**
+ * 
+ * @author wiklu
+ * @version 1.0
+ * @since today
+ * 
+ *
+ */
+
 public class Name {
 
+	public static final int ACONSTANT = 1;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Tsd");
+	}
+	public void Vector() {}
+	/**
+	 * Adding two double
+	 * <p>
+	 * Long description
+	 * @param r Input factor
+	 * @return Result Vector
+	 * </p>
+	 */
+	public int add(double r) {
+		return 0;
 	}
 
 }

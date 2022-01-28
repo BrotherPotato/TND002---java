395132ab50fcdc687d7c6ae08340a65a7a8b6c17
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-01-28 10:50:40 +0100

    asd

--- Hobby/src/course/Filess.java
+++ Hobby/src/course/Filess.java
@@ -1,11 +1,68 @@
 package course;
 
-public class Filess {
+import java.io.BufferedReader;
+import java.io.Writer;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-	public static void main(String[] args) {
+public class Filess extends Writer{
+
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println(System.getProperty("user.dir"));
+		BufferedReader b = new BufferedReader(new FileReader("text.txt"));
+		String fname = "";
+		File f = new File(fname);
+		f.createNewFile();
+		boolean overwrite = false;
+		//Writer w2 = new BufferedWriter(new FileWriter(f, overwrite));
+		//PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("foo.out")));
+		
+		/*
+		String s;
+		
+		while((s = b.readLine()) != null) {
+			System.out.println(s);
+		}
+		*/
+		/*
+		String out = b.readLine();
+		System.out.println(out);
+		out = b.readLine();
+		System.out.println(out);
+		*/
+		/*
+		do {
+			System.out.println("Input: ");
+			in = b.readLine();
+			System.out.println(in);
+		} while(!in.equals("end"));
+		*/
+		b.close();
+		
 	}
-	
+
+	@Override
+	public void write(char[] cbuf, int off, int len) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void flush() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 
 }
---
+++ Lessons/src/lesson2/Lesson2.java
@@ -0,0 +1,94 @@
+package lesson2;
+import java.io.*; import java.util.Random; import java.util.ArrayList;
+public class Lesson2 {
+
+	public static void main(String[] args) throws IOException {
+		
+				
+		BufferedReader freader, creader;
+		//BufferedWriter fwriter;
+		File file;
+		
+		// Task 1
+		
+		file = new File("Test.txt");
+		
+		if (file.createNewFile() && file.exists() && file.canRead() && file.canWrite()) {
+			System.out.println(file.getAbsolutePath());
+			//file.deleteOnExit();			
+		}
+				
+		// Task 2
+		
+		//fwriter = new BufferedWriter(new FileWriter(file));
+		Random myRandom = new Random();
+		
+		
+		int maxloop = myRandom.nextInt(10000);
+		//fwriter.write("Loop variable: " + maxloop + " "); fwriter.newLine();
+		for (int i=0; i < maxloop; i++) {
+		//	fwriter.write(String.valueOf(myRandom.nextInt(1000)+ " "));				
+		}
+		
+		//fwriter.flush(); // Test what happens if you don't flush.
+		
+		// Task 3
+		
+		creader = new BufferedReader(new InputStreamReader(System.in));
+		
+		ArrayList<String> strArr = new ArrayList<String>();
+		String rs;
+		
+		do {
+			rs = creader.readLine();
+			strArr.add(rs); 			
+		}while(!rs.equals("end"));
+		
+		strArr.remove(strArr.size()-1);
+		
+		for (int j=0; j < strArr.size(); j++) {
+		//	fwriter.write(strArr.get(j));
+		//	fwriter.newLine();
+		}
+		
+		//fwriter.flush();
+		
+		// Task 4
+		ArrayList<Integer> intArr = new ArrayList<Integer>();
+		
+		String[] splitted;
+		
+		freader = new BufferedReader(new FileReader(file));
+		
+		rs = freader.readLine();
+		splitted = rs.split(" +");
+		System.out.println(splitted[2]);
+		
+		while ((rs = freader.readLine())!=null) {
+			splitted = rs.split(" +");
+			for (int i=0; i < splitted.length; i++) {
+				try {intArr.add(Integer.valueOf(splitted[i]));}
+				catch(NumberFormatException ierr) {strArr.add(splitted[i]);}
+			}
+		}
+		
+		System.out.println(intArr.size());
+		System.out.println(strArr.size());
+		
+		freader.close();
+		
+		freader = new BufferedReader(new FileReader(file));
+		
+		// Task 5
+		
+		long length = file.length();
+		
+		while (file.length() == length) {}
+		
+		System.out.println("The file size has changed");
+		
+		//freader.close(); creader.close(); fwriter.close(); 
+		file.delete(); 
+	}
+
+}

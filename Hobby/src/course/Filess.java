d5e3b26eb17f9b35f9ae1ac9a8e9d730dd022ded
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-01-28 10:24:00 +0100

    df

---
+++ Hobby/src/course/Filess.java
@@ -0,0 +1,11 @@
+package course;
+
+public class Filess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(System.getProperty("user.dir"));
+	}
+	
+
+}

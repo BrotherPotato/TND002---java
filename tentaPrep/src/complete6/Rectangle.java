419f51e49f7963cbde3650be293bca0913ec3f64
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2022-03-14 19:40:33 +0100

    s

--- tentaPrep/src/complete6/Rectangle.java
+++ tentaPrep/src/complete6/Rectangle.java
@@ -3,7 +3,10 @@ package complete6;
 public class Rectangle extends Shape {
 	Rectangle(){
 		thePoints = new Point[4];
-		thePoints[0]=null; thePoints[1]=null; thePoints[2]=null; thePoints[3]=null;
+		thePoints[0]=null; 
+		thePoints[1]=null; 
+		thePoints[2]=null; 
+		thePoints[3]=null;
 	}
 	public void setNormal(double i1, double i2, double i3) {
 		area = Math.sqrt(i1*i1+i2*i2+i3*i3);
